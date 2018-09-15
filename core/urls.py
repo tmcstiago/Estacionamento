d7a5297b5dbc403bafa9e652758112324a9e17b3
Tiago Miguel <tmcstiago@gmail.com>
2018-09-14 23:56:48 -0300

    adicionando a opcao de excluir pessoa

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
@@ -7,9 +7,12 @@ urlpatterns = [
 
     path('pessoas/', views.lista_pessoas, name='core_lista_pessoas'),
     path('pessoa-novo/', views.pessoa_novo, name='core_pessoa_novo'),
+    re_path(r'^pessoa-update/(?P<id>\d+)/$', views.pessoa_update, name='core_update_pessoa'),
+    re_path(r'^pessoa-delete/(?P<id>\d+)/$', views.pessoa_delete, name='core_delete_pessoa'),
 
     path('veiculos/', views.lista_veiculos, name='core_lista_veiculos'),
     path('veiculo-novo', views.veiculo_novo, name='core_veiculo_novo'),
+    re_path(r'^veiculo-update/(?P<id>\d+)/$', views.veiculo_update, name='core_veiculo_update'),
 
     path('mov-rot/', views.lista_mov_rotativos, name='core_lista_mov_rotativos'),
     path('mov-rot-novo/', views.mov_rotativo_novo, name = 'core_mov_rotativo_novo'),
--- core/views.py
+++ core/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.shortcuts import render
 from .forms import (
     PessoaForm,
     VeiculoForm,
@@ -36,6 +35,27 @@ def pessoa_novo(request):
     return redirect('core_lista_pessoas')
 
 
+def pessoa_update(request, id):
+    pessoa = Pessoa.objects.get(id=id)
+    form = PessoaForm(request.POST or None, instance=pessoa)
+    data = {'pessoa': pessoa, 'form': form}
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('core_lista_pessoas')
+    else:
+        return render(request, 'core/update_pessoa.html', data)
+
+
+def pessoa_delete(request, id):
+    pessoa = Pessoa.objects.get(id=id)
+    if request.method == 'POST':
+        pessoa.delete()
+        return redirect('core_lista_pessoas')
+    else:
+        return render(request, 'core/delete_confirm.html', {'pessoa': pessoa})
+
+
 def lista_veiculos(request):
     veiculos = Veiculo.objects.all()
     form = VeiculoForm()
@@ -49,6 +69,21 @@ def veiculo_novo(request):
         form.save()
     return redirect('core_lista_veiculos')
 
+
+def veiculo_update(request, id):
+    veiculo = Veiculo.objects.get(id=id)
+    form = VeiculoForm(request.POST or None, instance=veiculo)
+    data = {'veiculo':veiculo, 'form':form}
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('core_lista_veiculos')
+    
+    else:
+        return render(request, 'core/update_veiculo.html', data)
+
+        
 
 def lista_mov_rotativos(request):
     mov_rot = MovRotativo.objects.all()
